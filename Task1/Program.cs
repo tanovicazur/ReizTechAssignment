c7b5b6a0b50b3dcd3f52704ea4a16b2f4b0dc529
azurt <azurt@DESKTOP-JL7A6ES>
2023-02-23 12:43:50 +0100

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Task1
+{
+    static void Main()
+    {
+        int hours;
+        int minutes;
+        int degrees;
+
+        do
+        {
+            Console.Write("Please enter hours: ");
+            hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter minutes: ");
+            minutes = Convert.ToInt32(Console.ReadLine());
+
+            if (hours > 24 || hours < 0 || minutes > 60 || minutes < 0)
+                Console.WriteLine("Invalid input. Please try again!" + "\n");
+
+        } while (hours > 24 || hours < 0 || minutes > 60 || minutes < 0);
+
+        if (hours > 12)
+            hours -= 12;
+
+        int x = Math.Abs(minutes - hours * 5);
+        degrees = x * 6;
+      
+        if (degrees > 180)
+            degrees = 360 - degrees;
+
+        Console.WriteLine("Lesser angle of two arrows is " + degrees + " degrees.");
+        
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Task2{
+    class Branch
+    {
+        List<Branch> branches;
+        public string name;
+        public Branch()
+        {
+            this.branches = new List<Branch>();
+            this.name = " ";
+        }
+
+        public Branch(List<Branch> branches, string name)
+        {
+            this.branches = branches;
+            this.name = name;
+        }
+
+        public Branch(string name)
+        {
+            this.branches = new List<Branch>();
+            this.name = name;
+        }
+
+        public List<Branch> getBranches()
+        {
+            return branches;
+        }
+
+    }
+
+    static void Main()
+    {
+        Branch obj1 = new Branch("1");
+        Branch obj2 = new Branch("2");
+        Branch obj3 = new Branch("3");
+        Branch obj4 = new Branch("4");
+        Branch obj5 = new Branch("5");
+        obj1.getBranches().Add(obj2);
+        obj1.getBranches().Add(obj3);
+        obj2.getBranches().Add(obj4);
+        obj3.getBranches().Add(obj5);
+        for(int i = 0; i < obj1.getBranches().Count; i++)
+        {
+            Console.WriteLine(obj1.getBranches().ElementAt(i).name);
+        }
+    }
+}
